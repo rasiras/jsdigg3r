9169e33f473a96c380c2f333f1b4ae1afe4a17dd
rasiras <rasiraskp@gmail.com>
2020-08-04 00:28:55 +0530

    Update jsdigger.sh

--- jsdigger.sh
+++ jsdigger.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "enter_recon_number":
 read varname
-mkdir /root/home/sniper/$varname
+mkdir /root/home/$varname
 for url in $(cat urls.txt);do
-wget -nv $url -P /root/home/sniper/$varname
+wget -nv $url -P /root/home/$varname
 done
-for file in /root/home/sniper/$varname/*; do
+for file in /root/home/$varname/*; do
 cat $file | grep -aoP "(?<=(\"|\'|\`))\/[a-zA-Z0-9_?&=\/\-\#\.]*(?=(\"|\'|\`))" | sort -u
 done
-rm -r /root/home/sniper/$varname
+rm -r /root/home/$varname
