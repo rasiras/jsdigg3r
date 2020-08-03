017abcf962f240f925dad86c9ea6dfda3b108ab7
rasiras <rasiraskp@gmail.com>
2020-08-03 23:27:21 +0530

    Create jsdigger.sh

---
+++ jsdigger.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "enter_domain":
+read varname
+mkdir /root/home/sniper/$varname
+for url in $(cat urls.txt);do
+wget -nv $url -P /root/home/sniper/$varname
+done
+for file in /root/home/sniper/$varname/*; do
+cat $file | grep -aoP "(?<=(\"|\'|\`))\/[a-zA-Z0-9_?&=\/\-\#\.]*(?=(\"|\'|\`))" | sort -u
+done
+rm -r /root/home/sniper/$varname
